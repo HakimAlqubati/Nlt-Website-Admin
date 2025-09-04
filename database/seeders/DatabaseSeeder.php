756eb29a72dfaa1cce52ef1e0bc3e2fe695be99f
HakimAlqubati <hakimahmed123321@gmail.com>
2025-09-05 01:06:46 +0300

    add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-            // FeatureSeeder::class,
-            // FaqSeeder::class,
-            // ChooseItemSeeder::class,
-            // TestimonialSeeder::class,
+            FeatureSeeder::class,
+            FaqSeeder::class,
+            ChooseItemSeeder::class,
+            TestimonialSeeder::class,
             TopSectionSeeder::class,
         ]);
     }
